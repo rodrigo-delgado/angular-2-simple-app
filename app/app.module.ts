1ed3c305c4d52aa09928798956bd0ef543447ed7
rodrigo-delgado <rod.delgado85@gmail.com>
2017-03-03 10:58:00 +1300

    add angular router

--- app/app.module.ts
+++ app/app.module.ts
@@ -7,9 +7,10 @@ import {NavbarComponent} from './components/navbar/navbar.component';
 import {JumbotronComponent} from './components/jumbotron/jumbotron.component';
 import {AboutComponent} from './components/pages/about.component';
 import {HomeComponent} from './components/pages/home.component';
+import {routing} from './app.routing';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, routing ],
 
   declarations: [ AppComponent,
                   NavbarComponent,
